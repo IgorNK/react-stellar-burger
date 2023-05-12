64c1a8dabc7b9db1f2c438e7363e198ed870569b
Igor Nazirov <mettbs@gmail.com>
2023-05-13 01:16:46 +1100

    More styling of the BurgerConstructor component.

--- src/components/burgerconstructor/burgerconstructor.jsx
+++ src/components/burgerconstructor/burgerconstructor.jsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
-import { ConstructorElement } from "@ya.praktikum/react-developer-burger-ui-components";
+import {
+  ConstructorElement,
+  DragIcon,
+  CurrencyIcon,
+  Button,
+} from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./burgerconstructor.module.css";
 
 const BurgerConstructor = (props) => {
@@ -60,6 +65,148 @@ const BurgerConstructor = (props) => {
       image_large: "https://code.s3.yandex.net/react/code/sauce-02-large.png",
       __v: 0,
     },
+    {
+      _id: "60666c42cc7b410027a1a9b4",
+      name: "Мясо бессмертных моллюсков Protostomia",
+      type: "main",
+      proteins: 433,
+      fat: 244,
+      carbohydrates: 33,
+      calories: 420,
+      price: 1337,
+      image: "https://code.s3.yandex.net/react/code/meat-02.png",
+      image_mobile: "https://code.s3.yandex.net/react/code/meat-02-mobile.png",
+      image_large: "https://code.s3.yandex.net/react/code/meat-02-large.png",
+      __v: 0,
+    },
+    {
+      _id: "60666c42cc7b410027a1a9b9",
+      name: "Соус традиционный галактический",
+      type: "sauce",
+      proteins: 42,
+      fat: 24,
+      carbohydrates: 42,
+      calories: 99,
+      price: 15,
+      image: "https://code.s3.yandex.net/react/code/sauce-03.png",
+      image_mobile: "https://code.s3.yandex.net/react/code/sauce-03-mobile.png",
+      image_large: "https://code.s3.yandex.net/react/code/sauce-03-large.png",
+      __v: 0,
+    },
+    {
+      _id: "60666c42cc7b410027a1a9b8",
+      name: "Соус фирменный Space Sauce",
+      type: "sauce",
+      proteins: 50,
+      fat: 22,
+      carbohydrates: 11,
+      calories: 14,
+      price: 80,
+      image: "https://code.s3.yandex.net/react/code/sauce-04.png",
+      image_mobile: "https://code.s3.yandex.net/react/code/sauce-04-mobile.png",
+      image_large: "https://code.s3.yandex.net/react/code/sauce-04-large.png",
+      __v: 0,
+    },
+    {
+      _id: "60666c42cc7b410027a1a9bc",
+      name: "Плоды Фалленианского дерева",
+      type: "main",
+      proteins: 20,
+      fat: 5,
+      carbohydrates: 55,
+      calories: 77,
+      price: 874,
+      image: "https://code.s3.yandex.net/react/code/sp_1.png",
+      image_mobile: "https://code.s3.yandex.net/react/code/sp_1-mobile.png",
+      image_large: "https://code.s3.yandex.net/react/code/sp_1-large.png",
+      __v: 0,
+    },
+    {
+      _id: "60666c42cc7b410027a1a9bb",
+      name: "Хрустящие минеральные кольца",
+      type: "main",
+      proteins: 808,
+      fat: 689,
+      carbohydrates: 609,
+      calories: 986,
+      price: 300,
+      image: "https://code.s3.yandex.net/react/code/mineral_rings.png",
+      image_mobile:
+        "https://code.s3.yandex.net/react/code/mineral_rings-mobile.png",
+      image_large:
+        "https://code.s3.yandex.net/react/code/mineral_rings-large.png",
+      __v: 0,
+    },
+    {
+      _id: "60666c42cc7b410027a1a9ba",
+      name: "Соус с шипами Антарианского плоскоходца",
+      type: "sauce",
+      proteins: 101,
+      fat: 99,
+      carbohydrates: 100,
+      calories: 100,
+      price: 88,
+      image: "https://code.s3.yandex.net/react/code/sauce-01.png",
+      image_mobile: "https://code.s3.yandex.net/react/code/sauce-01-mobile.png",
+      image_large: "https://code.s3.yandex.net/react/code/sauce-01-large.png",
+      __v: 0,
+    },
+    {
+      _id: "60666c42cc7b410027a1a9bd",
+      name: "Кристаллы марсианских альфа-сахаридов",
+      type: "main",
+      proteins: 234,
+      fat: 432,
+      carbohydrates: 111,
+      calories: 189,
+      price: 762,
+      image: "https://code.s3.yandex.net/react/code/core.png",
+      image_mobile: "https://code.s3.yandex.net/react/code/core-mobile.png",
+      image_large: "https://code.s3.yandex.net/react/code/core-large.png",
+      __v: 0,
+    },
+    {
+      _id: "60666c42cc7b410027a1a9be",
+      name: "Мини-салат Экзо-Плантаго",
+      type: "main",
+      proteins: 1,
+      fat: 2,
+      carbohydrates: 3,
+      calories: 6,
+      price: 4400,
+      image: "https://code.s3.yandex.net/react/code/salad.png",
+      image_mobile: "https://code.s3.yandex.net/react/code/salad-mobile.png",
+      image_large: "https://code.s3.yandex.net/react/code/salad-large.png",
+      __v: 0,
+    },
+    {
+      _id: "60666c42cc7b410027a1a9b3",
+      name: "Филе Люминесцентного тетраодонтимформа",
+      type: "main",
+      proteins: 44,
+      fat: 26,
+      carbohydrates: 85,
+      calories: 643,
+      price: 988,
+      image: "https://code.s3.yandex.net/react/code/meat-03.png",
+      image_mobile: "https://code.s3.yandex.net/react/code/meat-03-mobile.png",
+      image_large: "https://code.s3.yandex.net/react/code/meat-03-large.png",
+      __v: 0,
+    },
+    {
+      _id: "60666c42cc7b410027a1a9bf",
+      name: "Сыр с астероидной плесенью",
+      type: "main",
+      proteins: 84,
+      fat: 48,
+      carbohydrates: 420,
+      calories: 3377,
+      price: 4142,
+      image: "https://code.s3.yandex.net/react/code/cheese.png",
+      image_mobile: "https://code.s3.yandex.net/react/code/cheese-mobile.png",
+      image_large: "https://code.s3.yandex.net/react/code/cheese-large.png",
+      __v: 0,
+    },
   ]);
 
   const addIngredient = (item) => {
@@ -77,35 +224,55 @@ const BurgerConstructor = (props) => {
 
   const renderBun = (item, type) => {
     return (
-      <ConstructorElement
-        type={type}
-        isLocked={true}
-        text={item.name}
-        price={item.price}
-        thumbnail={item.image_mobile}
-      />
+      <li className="pl-8 pr-8">
+        <ConstructorElement
+          type={type}
+          isLocked={true}
+          text={item.name}
+          price={item.price}
+          thumbnail={item.image_mobile}
+        />
+      </li>
     );
   };
 
   const renderIngredient = (item) => {
     return item.type !== "bun" ? (
-      <ConstructorElement
-        text={item.name}
-        price={item.price}
-        thumbnail={item.image_mobile}
-      />
+      <li className={styles.draggable + " pl-8"}>
+        <div className={styles.dragHandle}>
+          <DragIcon />
+        </div>
+        <ConstructorElement
+          text={item.name}
+          price={item.price}
+          thumbnail={item.image_mobile}
+        />
+      </li>
     ) : null;
   };
 
   return (
-    <div className={styles.burgerconstructor + " pt-25"}>
-      {renderBun(getBun(ingredients), "top")}
-      <div className={styles.ingredientsContainer + " custom-scroll"}>
-        {ingredients.map((item) => {
-          return renderIngredient(item);
-        })}
+    <div className={styles.burgerconstructor + " mt-25 pl-4"}>
+      <ul className={styles.list}>
+        {renderBun(getBun(ingredients), "top")}
+        <li className={styles.ingredientsContainer + " custom-scroll pr-4"}>
+          <ul className={styles.list}>
+            {ingredients.map((item) => {
+              return renderIngredient(item);
+            })}
+          </ul>
+        </li>
+        {renderBun(getBun(ingredients), "bottom")}
+      </ul>
+      <div className={styles.checkoutContainer + " pr-8"}>
+        <div className={styles.totalContainer}>
+          <p className="text text_type_digits-medium">610</p>
+          <CurrencyIcon className={styles.priceIcon} />
+        </div>
+        <Button htmlType="button" type="primary" size="large">
+          Оформить заказ
+        </Button>
       </div>
-      {renderBun(getBun(ingredients), "bottom")}
     </div>
   );
 };
